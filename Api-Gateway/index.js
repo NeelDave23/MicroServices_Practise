499393d7cefcf4a76c820a696de4e6b0abee174e
neel <neel2003dave@gmail.com>
2024-02-26 18:23:30 +0530

    Adding Swagger UI

--- Api-Gateway/index.js
+++ Api-Gateway/index.js
@@ -23,14 +23,22 @@ for (const route in routes) {
   }
 }
 
-const sslServer = https.createServer(
-  {
-    key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
-    cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
-  },
-  app
-);
+// const sslServer = https.createServer(
+//   {
+//     key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
+//     cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+//   },
+//   app
+// );
 
-sslServer.listen(process.env.PORT, () => {
+// sslServer.listen(process.env.PORT, () => {
+//   console.log(` Port :- ${process.env.PORT}`);
+// });
+
+// app.listen(3000, "0.0.0.0", () => {
+//   console.log("Orders. Port :- 3000");
+// });
+
+app.listen(process.env.PORT, () => {
   console.log(` Port :- ${process.env.PORT}`);
 });
--- authentication/index.js
+++ authentication/index.js
@@ -5,14 +5,34 @@ const adminRouter = require("./routes/admin_route");
 require("./model/db");
 let cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const swaggerjsdoc = require("swagger-jsdoc");
+const swaggerui = require("swagger-ui-express");
+
 require("dotenv").config();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/users", router);
 app.use("/users/admin", adminRouter);
+app.get("/users", (req, res) => {
+  res.send("Authentication");
+});
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Microservices ",
+      version: "1.0.0",
+    },
+    servers: [{ url: "http://localhost:3002/users/" }],
+  },
+  apis: ["./routes/*.js"],
+};
+const spacs = swaggerjsdoc(options);
+app.use("/api-docs", swaggerui.serve, swaggerui.setup(spacs));
 app.listen(process.env.PORT, () => {
   console.log(` Port :- ${process.env.PORT}`);
 });
